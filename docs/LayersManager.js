261263cb600fa023fafd9ca252742abe3a52b10e
fred <fred@maccathou.local>
2020-11-08 22:11:48 +0100

    initial commit

--- docs/LayersManager.js
+++ docs/LayersManager.js
@@ -1,3 +1,29 @@
-function test() {
-    //document.getElementById('hello').style.display = 'none';
-}
+(function(){
+  initialize: function(buttons, options){
+
+    if(options){
+      L.Util.setOptions( this, options );
+    }
+
+    this._buildContainer();
+    this._buttons = [];
+
+    for(var i = 0; i < buttons.length; i++){
+      buttons[i]._bar = this;
+      buttons[i]._container = buttons[i].button;
+      this._buttons.push(buttons[i]);
+      this.container.appendChild(buttons[i].button);
+    }
+
+    },
+
+  onAdd: function () {
+    return this.container;
+  }
+
+});
+
+L.layersManager = function(/* args will pass automatically */){
+  var args = Array.prototype.concat.apply([L.Control.layersManager],arguments);
+  return new (Function.prototype.bind.apply(L.Control.layersManager, args));
+};
