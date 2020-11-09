100ffb4318e0f4c076f036204b077950e6729eed
fred <fred@maccathou.local>
2020-11-09 21:57:29 +0100

    initial commit

--- docs/LayersManager.js
+++ docs/LayersManager.js
@@ -27,6 +27,31 @@ L.Control.layersManager = L.Control.Layers.extend({
     options: {
         position: 'topright',
     },
+
+    initialize: function(baseLayers, groupedOverlays, options) {
+      var i,
+          j;
+      L.Util.setOptions(this, options);
+
+      this._layerControlInputs = [];
+      this._layers = [];
+      this._lastZIndex = 0;
+      this._handlingClick = false;
+      this._groupList = [];
+      this._domGroups = [];
+
+      /*for (i in baseLayers) {
+          for (var j in baseLayers[i].layers) {
+              this._addLayer(baseLayers[i].layers[j], j, baseLayers[i], false);
+          }
+      }
+
+      for (i in groupedOverlays) {
+          for (var j in groupedOverlays[i].layers) {
+              this._addLayer(groupedOverlays[i].layers[j], j, groupedOverlays[i], true);
+          }
+      }*/
+    }
 });
 L.Control.layersManager = function(baseLayers, overlays, options) {
     return new L.Control.layersManager(baseLayers, overlays, options);
