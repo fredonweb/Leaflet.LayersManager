96083acef75982578cd32e9dd2a74c5191790188
fredonweb <fredonweb@gmail.com>
2020-11-11 22:58:01 +0100

    initial commit

--- docs/LayersManager.js
+++ docs/LayersManager.js
@@ -49,43 +49,43 @@ L.Control.layersManager = L.Control.extend({
     listElement.onclick = loadMarkers;
     listElementLabel.innerHTML = '<input type="checkbox" /><span>GIE La Ville Autrement</span>';
 
-    let listElement2 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement2 = this.listElement2 = L.DomUtil.create('li', '', list);
     let listElementLabel2 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement2);
     listElementLabel2.innerHTML = '<input type="checkbox" /><span>Cadastre</span>';
 
-    let listElement3 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement3 = this.listElement3 = L.DomUtil.create('li', '', list);
     let listElementLabel3 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement3);
     listElementLabel3.innerHTML = '<input type="checkbox" /><span>PLU-H</span>';
 
-    let listElement4 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement4 = this.listElement4 = L.DomUtil.create('li', '', list);
     let listElementLabel4 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement4);
     listElementLabel4.innerHTML = '<input name="group1" type="radio" checked /><span>Zonage et réglement</span>';
 
-    let listElement5 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement5 = this.listElement5 = L.DomUtil.create('li', '', list);
     let listElementLabel5 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement5);
     listElementLabel5.innerHTML = '<input name="group1" type="radio" /><span>Orientations d\'Aménagement et de Programmation</span>';
 
-    let listElement6 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement6 = this.listElement6 = L.DomUtil.create('li', '', list);
     let listElementLabel6 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement6);
     listElementLabel6.innerHTML = '<input name="group1" type="radio" /><span>Secteurs de mixité sociale</span>';
 
-    let listElement7 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement7 = this.listElement7 = L.DomUtil.create('li', '', list);
     let listElementLabel7 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement7);
     listElementLabel7.innerHTML = '<input name="group1" type="radio" /><span>Emplacements réservés</span>';
 
-    let listElement8 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement8 = this.listElement8 = L.DomUtil.create('li', '', list);
     let listElementLabel8 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement8);
     listElementLabel8.innerHTML = '<input name="group1" type="radio" /><span>Hauteur de façade</span>';
 
-    let listElement9 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement9 = this.listElement9 = L.DomUtil.create('li', '', list);
     let listElementLabel9 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement9);
     listElementLabel9.innerHTML = '<input name="group1" type="radio" /><span>Taille de logement</span>';
 
-    let listElement10 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement10 = this.listElement10 = L.DomUtil.create('li', '', list);
     let listElementLabel10 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement10);
     listElementLabel10.innerHTML = '<input type="checkbox" /><span>Demandes de valeurs foncières</span>';
 
-    let listElement11 = this.listElement = L.DomUtil.create('li', '', list);
+    let listElement11 = this.listElement11 = L.DomUtil.create('li', '', list);
     let listElementLabel11 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement11);
     listElementLabel11.innerHTML = '<input type="checkbox" /><span>Sites et sols pollués BASOL</span>';
 
