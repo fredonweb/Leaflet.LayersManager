a63de4be0e4d865a42f47f6b81298d4e9b26e18e
fredonweb <fredonweb@gmail.com>
2020-11-15 23:22:22 +0100

    initial commit

--- docs/LayersManager.js
+++ docs/LayersManager.js
@@ -1,98 +1,132 @@
-/*(function (factory, window) {
+(function (factory, window) {
 
     // define an AMD module that relies on 'leaflet'
     if (typeof define === 'function' && define.amd) {
         define(['leaflet'], factory);
-
     // define a Common JS module that relies on 'leaflet'
     } else if (typeof exports === 'object') {
         module.exports = factory(require('leaflet'));
-    }
-
-    // attach your plugin to the global 'L' variable
-    if (typeof window !== 'undefined' && window.L) {
-        window.L.layersManager = factory(L);
+    } else {
+        // Assume Leaflet is loaded into global object L already
+        factory(L);
     }
 }(function (L) {
-    var myLayersManager = {};
-    // implement your plugin
+  //'use strict';
+  L.Control.layersManager = L.Control.extend({
+    options: {
+      position: 'topleft',
+      layers: [{
+        layerName: '',
+        inputName: '',
+        inputType: 'checkbox',
+        inputDisabled: false,
+        layerFunctionOn: '',
+        layerFunctionOff: ''
+      }]
+    },
+    initialize: function(options) {
+      L.Util.setOptions(this, options);
+      //const layersList = options.layers;
+      //console.log(layersList);
+    },
+    onAdd: function(map) {
 
-    // return your plugin when you are done
-    return myLayersManager;
-}, window));*/
+      // Container
+      let controlElementClassName = 'leaflet-layers-manager';
+      let controlElement = this.controlElement = L.DomUtil.create('div', controlElementClassName);
+      // title
+      let title = this.title = L.DomUtil.create('h6', controlElementClassName + '-title', controlElement);
+      title.innerHTML = '<i id="show-hide-btn" class="material-icons left">keyboard_arrow_up</i><span>Détails de la carte</span>';
+      title.id = 'show-hide';
 
-L.Control.layersManager = L.Control.extend({
-  options: {
-    position: 'topright'
-  },
-  initialize: function(options) {
-    L.Util.setOptions(this, options);
-  },
-  onAdd: function(map) {
+      // <ul>
+      let list = this.list = L.DomUtil.create('ul', controlElementClassName + '-list', controlElement);
+      list.id = 'leaflet-layers-manager-list';
+      list.style.display = 'none';
 
-    // Container
-    let controlElementClassName = 'leaflet-layers-manager';
-    let controlElement = this.controlElement = L.DomUtil.create('div', controlElementClassName);
-    // title
-    let title = this.title = L.DomUtil.create('h6', controlElementClassName + '-title', controlElement);
-    title.innerHTML = '<i id="show-hide-btn" class="material-icons left">keyboard_arrow_up</i><span>Détails de la carte</span>';
-    title.id = 'show-hide';
+      //li
+      for (let i = 0; i < this.options.layers.length; i++) {
+        let listElement = this.listElement = L.DomUtil.create('li', '', list);
 
-    // <ul>
-    let list = this.list = L.DomUtil.create('ul', controlElementClassName + '-list', controlElement);
-    list.id = 'leaflet-layers-manager-list';
-    list.style.display = 'none';
+        //label
+        let onOffClassName = '';
+        if (this.options.layers[i].inputType == 'checkbox') onOffClassName = 'layerIsOff';
+        let listElementLabel = this.listElementLabel = L.DomUtil.create('label', '', listElement);
+        listElementLabel.innerHTML = '<input name="' + this.options.layers[i].inputName + '" type="' + this.options.layers[i].inputType + '" />' +
+                                     '<span id="leaflet-layers-manager-entry-' + i + '" class="' + onOffClassName + '">' + this.options.layers[i].layerName + '</span>';
 
-    // <li>
-    let listElement = this.listElement = L.DomUtil.create('li', '', list);
-    let listElementLabel = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement);
-    listElement.onclick = loadMarkers;
-    listElementLabel.innerHTML = '<input type="checkbox" /><span>GIE La Ville Autrement</span>';
+        //input
+        if (this.options.layers[i].inputDisabled == true || listElementLabel.firstChild.type == 'radio') {
+          listElementLabel.firstChild.disabled = true;
+          listElementLabel.lastChild.classList.add('disabled');
+          listElementLabel.lastChild.style.pointerEvents = 'none';
+        }
 
-    let listElement2 = this.listElement2 = L.DomUtil.create('li', '', list);
-    let listElementLabel2 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement2);
-    listElementLabel2.innerHTML = '<input type="checkbox" /><span>Cadastre</span>';
+        //span
+        let listElementSpan = listElementLabel.lastChild;
+        if (this.options.layers[i].inputType == 'checkbox' && this.options.layers[i].inputName !== '') {
+          L.DomEvent.on(listElementSpan, 'click', function (e) {
+            this.onOffSubList('' + this.options.layers[i].inputName + '', '' + this.options.layers[i].layerFunctionOn + '', '' + this.options.layers[i].layerFunctionOff + '')
+          }, this);
+        }
+        if (this.options.layers[i].inputDisabled !== true) {
+          listElementSpan.setAttribute('onClick', '' + this.options.layers[i].layerFunctionOn + '');
+          if (this.options.layers[i].inputType = 'checkbox') {
+            L.DomEvent.on(listElementSpan, 'click', function (e) {
+              this.onOffElement('leaflet-layers-manager-entry-' + i + '', '' + this.options.layers[i].layerFunctionOn + '', '' + this.options.layers[i].layerFunctionOff + '');
+            }, this);
+          }
+        }
+      }
 
-    let listElement3 = this.listElement3 = L.DomUtil.create('li', '', list);
-    let listElementLabel3 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement3);
-    listElementLabel3.innerHTML = '<input type="checkbox" /><span>PLU-H</span>';
+      return controlElement;
 
-    let listElement4 = this.listElement4 = L.DomUtil.create('li', '', list);
-    let listElementLabel4 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement4);
-    listElementLabel4.innerHTML = '<input name="group1" type="radio" checked /><span>Zonage et réglement</span>';
+      },
+    onOffElement: function(id, functionOn, functionOff) {
+        let element = document.getElementById(id);
+          if (element.classList.contains('layerIsOff')) {
+            element.classList.add('layerIsOn');
+            element.classList.remove('layerIsOff');
+            element.setAttribute('onClick', '' + functionOff + '');
+          } else {
+            element.classList.add('layerIsOff');
+            element.classList.remove('layerIsOn');
+            element.setAttribute('onClick', '' + functionOn + '');
+          }
+      },
+    onOffSubList: function(groupName, functionOn, functionOff) {
+      let elements = document.getElementsByName('' + groupName + '');
+      for (let i = 0; i < elements.length; i++) {
+        if (elements[i].type == 'radio') {
+          if (elements[i].disabled) {
+            elements[i].disabled = false;
+            elements[i].nextSibling.classList.remove('disabled');
+            elements[i].nextSibling.style.pointerEvents = 'auto';
+          } else {
+            elements[i].disabled = true;
+            elements[i].checked = false;
+            elements[i].nextSibling.classList.add('disabled');
+            elements[i].nextSibling.style.pointerEvents = 'none';
+          }
+        } else {
+          if (elements[i].disabled) {
+            //elements[i].nextSibling.setAttribute('onClick', '' + functionOn + '');
+          } else {
+            //elements[i].nextSibling.setAttribute('onClick', '' + functionOff + '');
+          }
+        }
+      }
+    }
+  });
 
-    let listElement5 = this.listElement5 = L.DomUtil.create('li', '', list);
-    let listElementLabel5 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement5);
-    listElementLabel5.innerHTML = '<input name="group1" type="radio" /><span>Orientations d\'Aménagement et de Programmation</span>';
+  L.layersManager = function (options) {
+		return new L.Control.layersManager(options);
+	};
 
-    let listElement6 = this.listElement6 = L.DomUtil.create('li', '', list);
-    let listElementLabel6 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement6);
-    listElementLabel6.innerHTML = '<input name="group1" type="radio" /><span>Secteurs de mixité sociale</span>';
+	return L;
 
-    let listElement7 = this.listElement7 = L.DomUtil.create('li', '', list);
-    let listElementLabel7 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement7);
-    listElementLabel7.innerHTML = '<input name="group1" type="radio" /><span>Emplacements réservés</span>';
+}));
 
-    let listElement8 = this.listElement8 = L.DomUtil.create('li', '', list);
-    let listElementLabel8 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement8);
-    listElementLabel8.innerHTML = '<input name="group1" type="radio" /><span>Hauteur de façade</span>';
-
-    let listElement9 = this.listElement9 = L.DomUtil.create('li', '', list);
-    let listElementLabel9 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement9);
-    listElementLabel9.innerHTML = '<input name="group1" type="radio" /><span>Taille de logement</span>';
-
-    let listElement10 = this.listElement10 = L.DomUtil.create('li', '', list);
-    let listElementLabel10 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement10);
-    listElementLabel10.innerHTML = '<input type="checkbox" /><span>Demandes de valeurs foncières</span>';
-
-    let listElement11 = this.listElement11 = L.DomUtil.create('li', '', list);
-    let listElementLabel11 = this.listElementLabel = L.DomUtil.create('label', 'noClassName', listElement11);
-    listElementLabel11.innerHTML = '<input type="checkbox" /><span>Sites et sols pollués BASOL</span>';
-
-    return controlElement;
-
-  }
-});
-L.layersManager = function(options) {
-  return new L.Control.layersManager(options);
-};
+//L.layersManager = function(options) {
+//  return new L.Control.layersManager(options);
+//};
