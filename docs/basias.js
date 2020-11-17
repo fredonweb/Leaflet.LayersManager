17e7d99fa762fdf8557d645cbe49683ec0528a6c
fredonweb <36740595+fredonweb@users.noreply.github.com>
2020-11-17 12:06:23 +0100

    Update basias.js

--- docs/basias.js
+++ docs/basias.js
@@ -1,5 +1,10 @@
+https://www.georisques.gouv.fr/api/v1/sis?rayon=2000&latlon=4.8589028%2C45.7699061&page=1&page_size=100
 
+https://www.georisques.gouv.fr/services?LANGUAGE=fre&SERVICE=WFS&typenames=BASIAS_ADRESSE&service=WFS&request=GetFeature&version=2.0.0&outputFormat=GEOJSON&projection=WGS84&BBOX=44.00,4.90,45.00,5.00
+
+https://georisques.gouv.fr/services?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=BASIAS_ADRESSE&outputFormat=GEOJSON&projection=WGS84&BBox=803169.01,6015062.01,854580.01,6415173.01
 
 http://georisques.gouv.fr/services?LANGUAGE=fre&SERVICE=WFS&REQUEST=GetCapabilities
 
 http://fiches-risques.brgm.fr/georisques/basias-detaillee/"
+
